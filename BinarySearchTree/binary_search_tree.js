02154224c6ecc1777bfc3b2daf72a023ead3ffd7
Patil, Mangauda <abhay.patil@pearson.com>
2014-10-09 17:29:54 -0700

    jsDS-140910 : Binary Search Tree Skeleton implementation

---
+++ BinarySearchTree/binary_search_tree.js
@@ -0,0 +1,36 @@
+/**
+ * Created by abhaypatil on 9/12/14.
+ */
+function linkedList(init) {
+
+    /*
+     * Note: Node has been declared with var so it's a private variable now.
+     * Same goes to the 'root' variable.
+     */
+    var Node = function(value) {
+        this.left_child = null;
+        this.right_child = null;
+        this.value = value;
+    };
+
+    var root = new Node(init);
+
+    /*
+     * addNode can be accessed as a public method since it has been declared with 'this'
+     */
+    this.addNode = function(value) {
+
+    };
+
+    this.traverse = function() {
+
+    };
+
+    this.removeNode = function(value) {
+
+    };
+
+    this.getRoot = function() {
+        return root;
+    }
+}
